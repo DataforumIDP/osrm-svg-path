62dc17f65b6dcbbde6c47c641f0d4d334dcd7566
Daniel <daniel.vn.rus@gmail.com>
2025-08-12 19:38:30 +0300

    Add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'map-svg-path',
+            script: './src/server.js',
+            watch: true,
+            node_args: '--no-warnings',
+        },
+    ],
+}
+
+// /var/www/www-root/data/www/api.100q
